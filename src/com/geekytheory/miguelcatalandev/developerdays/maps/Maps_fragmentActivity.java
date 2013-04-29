c263d452d6c36186b039ef57777007bebd134796
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2013-04-29 13:11:34 +0200

    - Added places fragment - Minor bug fixed

---
+++ src/com/geekytheory/miguelcatalandev/developerdays/maps/Maps_fragmentActivity.java
@@ -0,0 +1,7 @@
+package com.geekytheory.miguelcatalandev.developerdays.maps;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class Maps_fragmentActivity extends SherlockFragmentActivity{
+
+}
