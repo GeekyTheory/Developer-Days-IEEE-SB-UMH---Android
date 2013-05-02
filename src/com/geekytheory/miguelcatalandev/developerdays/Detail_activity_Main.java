8cf28a411335477fa7bf61ac4afc78739032b9b2
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2013-05-02 19:57:17 +0200

    - Improved timetable and DetailView

--- src/com/geekytheory/miguelcatalandev/developerdays/Detail_activity_Main.java
+++ src/com/geekytheory/miguelcatalandev/developerdays/Detail_activity_Main.java
@@ -1,17 +1,76 @@
 package com.geekytheory.miguelcatalandev.developerdays;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
-public class Detail_activity_Main extends SherlockActivity{
+public class Detail_activity_Main extends SherlockActivity implements
+		OnClickListener {
+	
+	Bundle extras;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.detail_activity_detail);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		initialize();
 	}
-	
-	
+
+	private void initialize() {
+		extras = getIntent().getExtras();
+		if (extras != null) {
+
+			setTitle(extras.getString("title"));
+
+			TextView name = (TextView) findViewById(R.id.detail_textview_name);
+			name.setText(extras.getString("name"));
+
+			TextView buis = (TextView) findViewById(R.id.detail_textview_buis);
+			buis.setText(extras.getString("buis"));
+
+			TextView url = (TextView) findViewById(R.id.detail_textview_url);
+			url.setText(extras.getString("url"));
+			url.setOnClickListener(this);
+
+			TextView des = (TextView) findViewById(R.id.detail_textview_des);
+			des.setText(extras.getString("des"));
+
+			TextView time = (TextView) findViewById(R.id.detail_textview_time);
+			time.setText("Horario: " + extras.getString("time"));
+
+			TextView loc = (TextView) findViewById(R.id.detail_textview_location);
+			loc.setText("Localizaci—n: " + extras.getString("loc"));
+
+			ImageView profile = (ImageView) findViewById(R.id.detail_imageview_profile);
+			profile.setImageResource(extras.getInt("image",
+					R.drawable.no_photo_circle));
+
+		}
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case android.R.id.home:
+			onBackPressed();
+			break;
+		case R.id.detail_textview_url:
+			Intent intent = new Intent(Intent.ACTION_VIEW);
+			intent.setData(Uri.parse(extras.getString("url")));
+			startActivity(intent);
+			break;
+		case R.id.detail_imageview_location:
+			break;
+		}
+
+	}
 
 }
--- src/com/geekytheory/miguelcatalandev/developerdays/Main_Fragment_Timetable.java
+++ src/com/geekytheory/miguelcatalandev/developerdays/Main_Fragment_Timetable.java
@@ -9,9 +9,12 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class Main_Fragment_Timetable extends Fragment implements OnClickListener{
-	
-	Button button_1,button_2,button_3,button_4,button_5,button_6,button_7,button_8,button_9,button_10,button_11,button_12,button_13;
+public class Main_Fragment_Timetable extends Fragment implements
+		OnClickListener {
+
+	Button button_1, button_2, button_3, button_4, button_5, button_6,
+			button_7, button_8, button_9, button_10, button_11, button_12,
+			button_13,button_14,button_15;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
@@ -28,75 +31,205 @@ public class Main_Fragment_Timetable extends Fragment implements OnClickListener
 	}
 
 	private void initialize() {
-		button_1=(Button)getView().findViewById(R.id.timetable_button_tecnical);
+		button_1 = (Button) getView().findViewById(
+				R.id.timetable_button_sl);
 		button_1.setOnClickListener(this);
-		
-		button_2=(Button)getView().findViewById(R.id.timetable_button_linux);
+
+		button_2 = (Button) getView().findViewById(R.id.timetable_button_linux);
 		button_2.setOnClickListener(this);
-		
-		button_3=(Button)getView().findViewById(R.id.timetable_button_wp);
+
+		button_3 = (Button) getView().findViewById(R.id.timetable_button_wp);
 		button_3.setOnClickListener(this);
-		
-		button_4=(Button)getView().findViewById(R.id.timetable_button_otp);
+
+		button_4 = (Button) getView().findViewById(R.id.timetable_button_otp);
 		button_4.setOnClickListener(this);
-		
-		button_5=(Button)getView().findViewById(R.id.timetable_button_opencv);
+
+		button_5 = (Button) getView()
+				.findViewById(R.id.timetable_button_opencv);
 		button_5.setOnClickListener(this);
-		
-		button_6=(Button)getView().findViewById(R.id.timetable_button_wp2);
+
+		button_6 = (Button) getView().findViewById(R.id.timetable_button_wp2);
 		button_6.setOnClickListener(this);
-		
-		button_7=(Button)getView().findViewById(R.id.timetable_button_arduino);
+
+		button_7 = (Button) getView().findViewById(
+				R.id.timetable_button_arduino);
 		button_7.setOnClickListener(this);
-		
-		button_8=(Button)getView().findViewById(R.id.timetable_button_node);
+
+		button_8 = (Button) getView().findViewById(R.id.timetable_button_node);
 		button_8.setOnClickListener(this);
-		
-		button_9=(Button)getView().findViewById(R.id.timetable_button_money);
+
+		button_9 = (Button) getView().findViewById(R.id.timetable_button_money);
 		button_9.setOnClickListener(this);
-		
-		button_10=(Button)getView().findViewById(R.id.timetable_button_ieee);
+
+		button_10 = (Button) getView().findViewById(R.id.timetable_button_ieee);
 		button_10.setOnClickListener(this);
-		
-		button_11=(Button)getView().findViewById(R.id.timetable_button_intelec);
+
+		button_11 = (Button) getView().findViewById(
+				R.id.timetable_button_intelec);
 		button_11.setOnClickListener(this);
-		
-		button_12=(Button)getView().findViewById(R.id.timetable_button_chat);
+
+		button_12 = (Button) getView().findViewById(R.id.timetable_button_chat);
 		button_12.setOnClickListener(this);
-		
-		button_13=(Button)getView().findViewById(R.id.timetable_button_microsoft);
+
+		button_13 = (Button) getView().findViewById(
+				R.id.timetable_button_microsoft);
 		button_13.setOnClickListener(this);
+		
+		button_14 = (Button) getView().findViewById(
+				R.id.timetable_button_aisoy);
+		button_14.setOnClickListener(this);
+		
+		button_15 = (Button) getView().findViewById(
+				R.id.timetable_button_yuliop);
+		button_15.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		Intent intent = new Intent(getActivity(), Detail_activity_Main.class);
-		switch(v.getId()){
+		switch (v.getId()) {
 		case R.id.timetable_button_arduino:
+			intent.putExtra("name", getString(R.string.event_arduino_name));
+			intent.putExtra("image", R.drawable.mario_perez_geekytheory);
+			intent.putExtra("buis", getString(R.string.event_arduino_buis));
+			intent.putExtra("des", getString(R.string.event_arduino_des));
+			intent.putExtra("loc", getString(R.string.event_arduino_loc));
+			intent.putExtra("time", getString(R.string.event_arduino_time));
+			intent.putExtra("title", getString(R.string.event_arduino_title));
+			intent.putExtra("url", getString(R.string.event_arduino_url));
 			break;
 		case R.id.timetable_button_chat:
+			intent.putExtra("name", getString(R.string.event_chat_name));
+			intent.putExtra("image", R.drawable.miguel_herrera_yuilop);
+			intent.putExtra("buis", getString(R.string.event_chat_buis));
+			intent.putExtra("des", getString(R.string.event_chat_des));
+			intent.putExtra("loc", getString(R.string.event_chat_loc));
+			intent.putExtra("time", getString(R.string.event_chat_time));
+			intent.putExtra("title", getString(R.string.event_chat_title));
+			intent.putExtra("url", getString(R.string.event_chat_url));
 			break;
 		case R.id.timetable_button_ieee:
+			intent.putExtra("name", getString(R.string.event_ieee_name));
+			intent.putExtra("buis", getString(R.string.event_ieee_buis));
+			intent.putExtra("des", getString(R.string.event_ieee_des));
+			intent.putExtra("loc", getString(R.string.event_ieee_loc));
+			intent.putExtra("time", getString(R.string.event_ieee_time));
+			intent.putExtra("title", getString(R.string.event_ieee_title));
+			intent.putExtra("url", getString(R.string.event_ieee_url));
 			break;
 		case R.id.timetable_button_intelec:
+			intent.putExtra("name", getString(R.string.event_intelec_name));
+			intent.putExtra("buis", getString(R.string.event_intelec_buis));
+			intent.putExtra("des", getString(R.string.event_intelec_des));
+			intent.putExtra("loc", getString(R.string.event_intelec_loc));
+			intent.putExtra("time", getString(R.string.event_intelec_time));
+			intent.putExtra("title", getString(R.string.event_intelec_title));
+			intent.putExtra("url", getString(R.string.event_intelec_url));
 			break;
 		case R.id.timetable_button_linux:
+			intent.putExtra("name", getString(R.string.event_linux_name));
+			intent.putExtra("image", R.drawable.david_ubeda_umh);
+			intent.putExtra("buis", getString(R.string.event_linux_buis));
+			intent.putExtra("des", getString(R.string.event_linux_des));
+			intent.putExtra("loc", getString(R.string.event_linux_loc));
+			intent.putExtra("time", getString(R.string.event_linux_time));
+			intent.putExtra("title", getString(R.string.event_linux_title));
+			intent.putExtra("url", getString(R.string.event_linux_url));
 			break;
 		case R.id.timetable_button_microsoft:
+			intent.putExtra("name", getString(R.string.event_microsoft_name));
+			intent.putExtra("buis", getString(R.string.event_microsoft_buis));
+			intent.putExtra("des", getString(R.string.event_microsoft_des));
+			intent.putExtra("loc", getString(R.string.event_microsoft_loc));
+			intent.putExtra("time", getString(R.string.event_microsoft_time));
+			intent.putExtra("title", getString(R.string.event_microsoft_title));
+			intent.putExtra("url", getString(R.string.event_microsoft_url));
 			break;
 		case R.id.timetable_button_money:
+			intent.putExtra("name", getString(R.string.event_money_name));
+			intent.putExtra("image", R.drawable.asun_vicente_umh);
+			intent.putExtra("buis", getString(R.string.event_money_buis));
+			intent.putExtra("des", getString(R.string.event_money_des));
+			intent.putExtra("loc", getString(R.string.event_money_loc));
+			intent.putExtra("time", getString(R.string.event_money_time));
+			intent.putExtra("title", getString(R.string.event_money_title));
+			intent.putExtra("url", getString(R.string.event_money_url));
 			break;
 		case R.id.timetable_button_node:
+			intent.putExtra("name", getString(R.string.event_node_name));
+			intent.putExtra("buis", getString(R.string.event_node_buis));
+			intent.putExtra("des", getString(R.string.event_node_des));
+			intent.putExtra("loc", getString(R.string.event_node_loc));
+			intent.putExtra("time", getString(R.string.event_node_time));
+			intent.putExtra("title", getString(R.string.event_node_title));
+			intent.putExtra("url", getString(R.string.event_node_url));
 			break;
 		case R.id.timetable_button_opencv:
+			intent.putExtra("name", getString(R.string.event_opencv_name));
+			intent.putExtra("image", R.drawable.jesualdo_ros_geekytheory);
+			intent.putExtra("buis", getString(R.string.event_opencv_buis));
+			intent.putExtra("des", getString(R.string.event_opencv_des));
+			intent.putExtra("loc", getString(R.string.event_opencv_loc));
+			intent.putExtra("time", getString(R.string.event_opencv_time));
+			intent.putExtra("title", getString(R.string.event_opencv_title));
+			intent.putExtra("url", getString(R.string.event_opencv_url));
 			break;
 		case R.id.timetable_button_otp:
+			intent.putExtra("name", getString(R.string.event_otp_name));
+			intent.putExtra("buis", getString(R.string.event_otp_buis));
+			intent.putExtra("des", getString(R.string.event_otp_des));
+			intent.putExtra("loc", getString(R.string.event_otp_loc));
+			intent.putExtra("time", getString(R.string.event_otp_time));
+			intent.putExtra("title", getString(R.string.event_otp_title));
+			intent.putExtra("url", getString(R.string.event_otp_url));
 			break;
-		case R.id.timetable_button_tecnical:
+		case R.id.timetable_button_sl:
+			intent.putExtra("name", getString(R.string.event_SL_name));
+			intent.putExtra("image", R.drawable.isidro_perez);
+			intent.putExtra("buis", getString(R.string.event_SL_buis));
+			intent.putExtra("des", getString(R.string.event_SL_des));
+			intent.putExtra("loc", getString(R.string.event_SL_loc));
+			intent.putExtra("time", getString(R.string.event_SL_time));
+			intent.putExtra("title", getString(R.string.event_SL_title));
+			intent.putExtra("url", getString(R.string.event_SL_url));
+			break;
+		case R.id.timetable_button_aisoy:
+			intent.putExtra("name", getString(R.string.event_aisoy_name));
+			intent.putExtra("image", R.drawable.diego_garcia_aisoy);
+			intent.putExtra("buis", getString(R.string.event_aisoy_buis));
+			intent.putExtra("des", getString(R.string.event_aisoy_des));
+			intent.putExtra("loc", getString(R.string.event_aisoy_loc));
+			intent.putExtra("time", getString(R.string.event_aisoy_time));
+			intent.putExtra("title", getString(R.string.event_aisoy_title));
+			intent.putExtra("url", getString(R.string.event_aisoy_url));
+			break;
+		case R.id.timetable_button_yuliop:
+			intent.putExtra("name", getString(R.string.event_yuilop_name));
+			intent.putExtra("buis", getString(R.string.event_yuilop_buis));
+			intent.putExtra("des", getString(R.string.event_yuilop_des));
+			intent.putExtra("loc", getString(R.string.event_yuilop_loc));
+			intent.putExtra("time", getString(R.string.event_yuilop_time));
+			intent.putExtra("title", getString(R.string.event_yuilop_title));
+			intent.putExtra("url", getString(R.string.event_yuilop_url));
 			break;
 		case R.id.timetable_button_wp:
+			intent.putExtra("name", getString(R.string.event_wp_name));
+			intent.putExtra("image", R.drawable.dachi_geekytheory);
+			intent.putExtra("buis", getString(R.string.event_wp_buis));
+			intent.putExtra("des", getString(R.string.event_wp_des));
+			intent.putExtra("loc", getString(R.string.event_wp_loc));
+			intent.putExtra("time", getString(R.string.event_wp_time));
+			intent.putExtra("title", getString(R.string.event_wp_title));
+			intent.putExtra("url", getString(R.string.event_wp_url));
 			break;
 		case R.id.timetable_button_wp2:
+			intent.putExtra("name", getString(R.string.event_wp2_name));
+			intent.putExtra("buis", getString(R.string.event_wp2_buis));
+			intent.putExtra("des", getString(R.string.event_wp2_des));
+			intent.putExtra("loc", getString(R.string.event_wp2_loc));
+			intent.putExtra("time", getString(R.string.event_wp2_time));
+			intent.putExtra("title", getString(R.string.event_wp2_title));
+			intent.putExtra("url", getString(R.string.event_wp2_url));
 			break;
 		}
 		startActivity(intent);
